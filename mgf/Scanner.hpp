8c8a78719b573af2c18cdd3784b44a20396f5b0e
Krozark <maxime.barbier1991@gmail.com>
2013-11-27 14:54:56 +0100

    add doc to Scanner

--- mgf/Scanner.hpp
+++ mgf/Scanner.hpp
@@ -17,24 +17,37 @@ namespace mgf
     class Parser;
     //typename Parser::semantic_type;
 
+    /***
+     * \brief The lexer that tokenize the input
+     */
     class Scanner : public yyFlexLexer
     {
         public:
+            /**
+             * \brief Construct from a input
+             * \param in MGF imput
+             */
             Scanner(std::istream& in);
             Scanner(const Scanner&) = delete;
             Scanner& operator=(const Scanner&) = delete;
 
-            // save the pointer to yylval so we can change it, and invoke scanner
+            /**
+             * \breief save the pointer to yylval so we can change it, and invoke scanner
+             * \parap lval For Lex/bison
+             * \return 0if all input is parse, MGF_END_IONS if end of ion, else, error
+             */
             int yylex(mgf::Parser::semantic_type *lval);
 
         protected:
 
         private:
-            // Scanning function created by Flex; make this private to force usage
-			// of the overloaded method so we can get a pointer to Bison's yylval
+            /**
+             * \breief Scanning function created by Flex; make this private to force usage
+             * of the overloaded method so we can get a pointer to Bison's yylval
+             */
             int yylex();
             /* yyval ptr */
-            mgf::Parser::semantic_type *yylval;
+            mgf::Parser::semantic_type *yylval; ///< For Lex/bison
     };
 }
 #endif
