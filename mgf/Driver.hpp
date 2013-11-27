627d1689a00a4c2d57f56c2da47b4551b145fc9c
Krozark <maxime.barbier1991@gmail.com>
2013-11-27 14:55:10 +0100

    add doc to Driver

--- mgf/Driver.hpp
+++ mgf/Driver.hpp
@@ -14,34 +14,77 @@ namespace mgf
     class Analyse;
     class Spectrum;
 
+    /**
+     * A class that have to be use to parse a MGF input
+     */
     class Driver
     {
         public:
+            /**
+             * \brief Construct a Driver from a stream
+             * \param in input stream
+             */
             Driver(std::istream& in);
+
             Driver(const Driver&) = delete;
             Driver& operator=(const Driver&) = delete;
 
+            /**
+             * \brief Destructor
+             */
             ~Driver();
 
+            /**
+             * \brief Parse all the input (until \0)
+             * \result A analyse tha contain all the datas
+             */
             Analyse parse();
 
+            /**
+             * \brief PArse only the next spectrum
+             * \result a pointer to the Spectrum. if nullptr is recive, all the input have been pase. You have to manualy delete the Spectrum
+             */
             Spectrum* next();
 
+            /**
+             * \brief Parse a input
+             * \param in The input stream to parse
+             * \result The analyse tha contain all the datas
+             */
             static Analyse parse(std::istream& in);
+
+            /**
+             * \brief Parse a input, and add the data parse to the analyse
+             * \param in Input to parse
+             * \param a Analyse where data will be saves
+             * \result number of spectrum parsed
+             */
             static int parse(std::istream& in,Analyse& a);
             
+            /**
+             * \brief Parse a file and return a Analyse
+             * \param filename the mgf file name
+             * \return A Analyse tha contain all the datas parsed
+             */
             static Analyse parse_file(const std::string& filename);
+
+            /**
+             * \brief Parse a file, and store data in the analyse
+             * \param filename file to parse
+             * \param a Analyse where data will be saved
+             * \result number of spectrum parsed
+             */
             static int parse_file(const std::string& filename,Analyse& a);
 
              
         private:
             friend class Parser;
                 
-            Scanner scanner;
-            Parser parser;
+            Scanner scanner; ///< The lexer
+            Parser parser; ///< The parser
 
-            mgf::GlobalHeader header;
-            mgf::Spectrum currentSpectrum;
+            mgf::GlobalHeader header; ///< the tmp global header 
+            mgf::Spectrum currentSpectrum; ///< the current spectrum parsed
     };
 }
 #endif
