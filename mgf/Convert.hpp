b6c4737100034857826087a8c93cef8a9f3a348e
Krozark <maxime.barbier1991@gmail.com>
2013-11-27 14:40:53 +0100

    add doc to Convert

--- mgf/Convert.hpp
+++ mgf/Convert.hpp
@@ -3,6 +3,9 @@
 
 namespace mgf
 {
+    /**
+     * \brief A class used to convert units, and to all maths
+     */
     class Convert
     {
         public:
@@ -10,10 +13,16 @@ namespace mgf
             Convert(const Convert&) = delete;
             Convert& operator=(const Convert&) = delete;
 
-            static constexpr float MH = 1.00794f; ///< Masse d'un Hydrogène en Da
-            static constexpr float MO = 15.99943f; ///< masse d'un carbone en Da
-            static constexpr float MH2O = (MO + 2*MH); ///< masse de H2O en Da
+            static constexpr float MH = 1.00794f; ///< Mass of Hydrogen in Da
+            static constexpr float MO = 15.99943f; ///< mass of carbon in Da
+            static constexpr float MH2O = (MO + 2*MH); ///< mass of H2O if Da
             
+            /**
+             * \brief convert a mz  in mass
+             * \param mz the M/Z ratio
+             * \param charge The charge (Z)
+             * \result the calculated mass in Da
+             */
             static constexpr double mz_to_masse(const double mz,const int charge){return mz*charge - charge*MH;}
 
         private:
