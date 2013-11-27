6cc0f17037f20b482481d9770068fc59680651bc
Krozark <maxime.barbier1991@gmail.com>
2013-11-27 14:36:29 +0100

    add GlobalHeader doc

--- mgf/GlobalHeader.hpp
+++ mgf/GlobalHeader.hpp
@@ -7,48 +7,193 @@
 
 namespace mgf
 {
+    /**
+     * \brief A class to store all Analyse meta data
+     * Note : Not all data are save (not use), even if there is function to set them.
+     * A message is print in that case
+     */
     class GlobalHeader
     {
         public:
             GlobalHeader(const GlobalHeader&) = delete;
             GlobalHeader& operator=(const GlobalHeader&) = delete;
-
+            
+            /**
+             * \brief Constructor. Do not set any default value to datas
+             */
             GlobalHeader();
+
+            /**
+             * \brief Default move constructor
+             */
             GlobalHeader(GlobalHeader&& tmp) = default;
 
+
+            /**
+             * \brief Not used
+             */
             void setAccession(std::list<double>& l);
-            void setCharge(std::list<int>& l);
+
+            /**
+             * \brief Not used
+             * \params charges List of possible charges for spectrums
+             */
+            void setCharge(std::list<int>& charges);
+
+            /**
+             * \brief Not used
+             */
             void setCle(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setCom(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setComp(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setCutout(std::list<int>& l);
+
+            /**
+             * \brief Not used
+             */
             void setDb(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setDecoy(int i);
+
+            /**
+             * \brief Not used
+             */
             void setErrorTolerant(int i);
+
+            /**
+             * \brief Not used
+             */
             void setFormat(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setFrames(std::list<int>& l);
+
+            /**
+             * \brief Not used
+             */
             void setInstrument(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setItMods(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setItOl(double d);
+
+            /**
+             * \brief Not used
+             */
             void setItOlU(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setMass(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setMods(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setMultiSiteMods(int i);
+
+            /**
+             * \brief Not used
+             */
             void setPepIsotopeError(int i);
+
+            /**
+             * \brief Not used
+             */
             void setPfa(int i);
+
+            /**
+             * \brief Not used
+             */
             void setPrecursor(double d);
+
+            /**
+             * \brief Not used
+             */
             void setQuantitation(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setRepport(int i);
+
+            /**
+             * \brief Not used
+             */
             void setReptype(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setSearch(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setSeg(double d);
+
+            /**
+             * \brief Not used
+             */
             void setTaxonomy(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setTol(double d);
+
+            /**
+             * \brief Not used
+             */
             void setTolU(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setUser(int i);
+
+            /**
+             * \brief Not used
+             */
             void setUserMail(std::string& s);
+
+            /**
+             * \brief Not used
+             */
             void setUserName(std::string& s);
             
+
+            /**
+             * \brief Debug print. Print all stored datas
+             */
             void __print__(std::ostream& stream)const;
 
         protected:
